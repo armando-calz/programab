f49d08e069606c3408118ad03f5e446ece19d87a
Armando Calzada <armcalz@outlook.com>
2021-01-05 20:21:14 -0600

    añadiendo curva superior derecha

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,3 +1,38 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
-console.log(lienzo)
+var lineas = 30;
+var l = 0;
+var j = 0;
+var yi, xf;
+var col = "#AAF"
+
+for(l=0; l < lineas; l++)
+{
+yi = 10 * l;
+xf = 10 * (l + 1);
+dibujarLinea(col, 1, yi, xf, 299);
+console.log("linea" + l);
+}
+
+for(j=0; j < lineas; j++)
+{
+yi = 10 * j;
+xf = 10 * (j + 1);
+dibujarLinea(col, 299, yi, xf, 1);
+}
+
+dibujarLinea (col, 1, 1, 1, 299)
+dibujarLinea (col, 1, 299, 299, 299)
+
+dibujarLinea (col, 299, 1, 1, 1)
+dibujarLinea (col, 299, 299, 299, 1)
+
+function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(x_inicial,y_inicial);
+  lienzo.lineTo(x_final,y_final);
+  lienzo.stroke();
+  lienzo.closePath();
+}
