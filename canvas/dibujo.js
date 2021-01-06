8ee96bb258c655c2cd99b6d6036ff12ac453115f
Armando Calzada <armcalz@outlook.com>
2021-01-05 21:46:55 -0600

    dibujo.js mejorado

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -2,30 +2,20 @@ var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 var lineas = 30;
 var l = 0;
-var j = 0;
 var yi, xf;
 var col = "#AAF"
 
-for(l=0; l < lineas; l++)
+while (l < lineas)
 {
-yi = 10 * l;
-xf = 10 * (l + 1);
-dibujarLinea(col, 1, yi, xf, 299);
-console.log("linea" + l);
+xf = 10 * (l + 1)
+yi = l * 10
+
+dibujarLinea(col,0,xf,yi,300);
+dibujarLinea(col,0,xf,300-yi,0);
+dibujarLinea(col,300,xf,yi,0);
+dibujarLinea(col,300,300-xf,yi,300);
+l++;
 }
-
-for(j=0; j < lineas; j++)
-{
-yi = 10 * j;
-xf = 10 * (j + 1);
-dibujarLinea(col, 299, yi, xf, 1);
-}
-
-dibujarLinea (col, 1, 1, 1, 299)
-dibujarLinea (col, 1, 299, 299, 299)
-
-dibujarLinea (col, 299, 1, 1, 1)
-dibujarLinea (col, 299, 299, 299, 1)
 
 function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final)
 {
