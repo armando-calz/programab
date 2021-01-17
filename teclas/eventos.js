631c9624bd4cf90685e2c82b764e141d850c66cb
Armando Calzada <armcalz@outlook.com>
2021-01-16 21:44:58 -0600

    comenzando switch de flechas

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -1,6 +1,27 @@
-document.addEventListener("keydown", dibujarTeclado);
+var teclas = { UP: 38, DOWN: 40, LEFT: 37, RIGHT: 39 };
 
-function dibujarTeclado()
+console.log(teclas);
+
+document.addEventListener("keyup", dibujarTeclado);
+
+function dibujarTeclado(evento)
 {
-  alert("AGAR")
+  switch (evento.keyCode)
+  {
+    case teclas.UP:
+    console.log("agar arriba");
+    break;
+    case teclas.DOWN:
+    console.log("agar abajo");
+    break;
+    case teclas.LEFT:
+    console.log("agar izquerda");
+    break;
+    case teclas.RIGHT:
+    console.log("agar derecha");
+    break;
+    default:
+    console.log("agar otra");
+    break
+  }
 }
