a82c8e4dfd2269ce48708f84cf47a06152636f13
Armando Calzada <armcalz@outlook.com>
2021-01-18 20:46:20 -0600

    flechas.html terminado

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -2,26 +2,48 @@ var teclas = { UP: 38, DOWN: 40, LEFT: 37, RIGHT: 39 };
 
 console.log(teclas);
 
-document.addEventListener("keyup", dibujarTeclado);
+document.addEventListener("keydown", dibujarTeclado);
+var c = document.getElementById("areaDeDibujo");
+var paper = c.getContext("2d");
+var x = 150;
+var y = 150;
 
+dibujarLinea("blue", 149, 149, 151, 151, paper);
+
+function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final, lienzo)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.lineWidth = 2;
+  lienzo.moveTo(x_inicial,y_inicial);
+  lienzo.lineTo(x_final,y_final);
+  lienzo.stroke();
+  lienzo.closePath();
+}
 function dibujarTeclado(evento)
 {
+  var colorcito = "blue";
+  var movimiento = 10;
   switch (evento.keyCode)
   {
     case teclas.UP:
-    console.log("agar arriba");
+    dibujarLinea(colorcito, x, y, x, y - movimiento, paper);
+    y = y - movimiento;
     break;
     case teclas.DOWN:
-    console.log("agar abajo");
+    dibujarLinea(colorcito, x, y, x, y + movimiento, paper);
+    y = y + movimiento;
     break;
     case teclas.LEFT:
-    console.log("agar izquerda");
+    dibujarLinea(colorcito, x, y, x - movimiento, y, paper);
+    x = x - movimiento;
     break;
     case teclas.RIGHT:
-    console.log("agar derecha");
+    dibujarLinea(colorcito, x, y, x + movimiento, y, paper);
+    x = x + movimiento;
     break;
     default:
     console.log("agar otra");
-    break
+    break;
   }
 }
