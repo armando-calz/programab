446b249e33613eada75bed8fd6d35709d3d922da
Armando Calzada <armcalz@outlook.com>
2021-01-23 22:58:04 -0600

    ordenando variables y añadiendo h

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -1,6 +1,5 @@
-var teclas = { UP: 38, DOWN: 40, LEFT: 37, RIGHT: 39, SPACE: 32};
-
-console.log(teclas);
+var teclas = { UP:38, DOWN:40, LEFT:37, RIGHT:39, SPACE:32};
+var grozor = { TECLADO:2, ESPACIO:0.1, MARCO:5};
 
 document.addEventListener("mousedown",comenzarMouse)
 document.addEventListener("mousemove",dibujarMouse)
@@ -9,17 +8,15 @@ document.addEventListener("keydown", dibujarTeclado);
 var c = document.getElementById("areaDeDibujo");
 var paper = c.getContext("2d");
 var colorLinea = "white";
-var grozorTeclado = 2;
-var grozorMarco = 5;
 var estado = 0;
 var x = 710;
 var y = 300;
 
 dibujarLinea(colorLinea, x - 1, y - 1, x + 1, y + 1, paper);
-dibujarLinea(colorLinea, 1380, 1, 1380, 599, paper, grozorMarco);
-dibujarLinea(colorLinea, 40, 1, 1380, 1, paper, grozorMarco);
-dibujarLinea(colorLinea, 40, 1, 40, 599, paper, grozorMarco);
-dibujarLinea(colorLinea, 40, 599, 1380, 599, paper, grozorMarco);
+dibujarLinea(colorLinea, 1380, 1, 1380, 599, paper, grozor.MARCO);
+dibujarLinea(colorLinea, 40, 1, 1380, 1, paper, grozor.MARCO);
+dibujarLinea(colorLinea, 40, 1, 40, 599, paper, grozor.MARCO);
+dibujarLinea(colorLinea, 40, 599, 1380, 599, paper, grozor.MARCO);
 
 
 function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final, lienzo, grozor)
@@ -41,7 +38,7 @@ function comenzarMouse(evento)
 function dibujarMouse(evento)
 {
   if (estado == 2) {
-  dibujarLinea(colorLinea, xt, yt, evento.layerX, evento.layerY, paper, grozorTeclado)
+  dibujarLinea(colorLinea, xt, yt, evento.layerX, evento.layerY, paper, grozor.TECLADO)
   }
   xt = evento.layerX;
   yt = evento.layerY;
@@ -62,23 +59,23 @@ function dibujarTeclado(evento)
   switch (evento.keyCode)
   {
     case teclas.UP:
-    dibujarLinea(colorcito, x, y, x, y - movimiento, paper, grozorTeclado);
+    dibujarLinea(colorcito, x, y, x, y - movimiento, paper, grozor.TECLADO);
     y = y - movimiento;
     break;
     case teclas.DOWN:
-    dibujarLinea(colorcito, x, y, x, y + movimiento, paper, grozorTeclado);
+    dibujarLinea(colorcito, x, y, x, y + movimiento, paper, grozor.TECLADO);
     y = y + movimiento;
     break;
     case teclas.LEFT:
-    dibujarLinea(colorcito, x, y, x - movimiento, y, paper, grozorTeclado);
+    dibujarLinea(colorcito, x, y, x - movimiento, y, paper, grozor.TECLADO);
     x = x - movimiento;
     break;
     case teclas.RIGHT:
-    dibujarLinea(colorcito, x, y, x + movimiento, y, paper, grozorTeclado);
+    dibujarLinea(colorcito, x, y, x + movimiento, y, paper, grozor.TECLADO);
     x = x + movimiento;
     break;
     case teclas.SPACE:
-    dibujarLinea(espacio, x, y, x + movEspacio, y, paper, grozorTeclado);
+    dibujarLinea(espacio, x, y, x + movEspacio, y, paper, grozor.ESPACIO);
     x = x + movEspacio;
     break;
     default:
