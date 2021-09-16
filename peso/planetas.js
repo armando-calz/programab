bbe674a8599a66487ad55749dd0b408724e934ae
Armando Calzada <armcalz@outlook.com>
2021-09-15 23:33:52 -0500

    planetas update

--- peso/planetas.js
+++ peso/planetas.js
@@ -1,18 +1,47 @@
 var g_tierra = 9.81;
 var g_marte = 3.7;
 var g_jupiter = 24.8;
+var g_sol = 274;
 
 function funMarte()
 {
   peso = document.getElementById("textoPeso").value;
   peso_final = parseInt(peso * g_marte / g_tierra);
-  swal("","Tu peso en Marte es de: "+peso_final+"kg","success")
-  console.log(peso);
+  if (peso>0)
+  {
+    swal("","Tu peso en Marte es de: "+peso_final+"kg","success")
+    console.log(peso);
+  }
+  else
+  {
+    swal("","ingresa tu peso","error")
+  }
 }
 function funJup()
 {
   peso = document.getElementById("textoPeso").value;
   peso_final = parseInt(peso * g_jupiter / g_tierra);
-  swal("","Tu peso en Jupiter es de: "+peso_final+"kg","success")
-  console.log(peso);
+  if (peso>0)
+  {
+    swal("","Tu peso en Jupiter es de: "+peso_final+"kg","success")
+    console.log(peso)
+  }
+  else
+  {
+    swal("","ingresa tu peso","error")
+  }
+}
+function funSol()
+{
+  peso = document.getElementById("textoPeso").value;
+  peso_final = parseInt(peso * g_sol / g_tierra);
+  if (peso>0)
+  {
+    swal("","Tu peso en el sol es de: "+peso_final+"kg","success")
+    console.log(peso)
+  }
+  else
+  {
+    swal("","ingresa tu peso","error")
+  }
 }
